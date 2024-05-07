1345a97c59c1fef7156b7d34fba87e1fa2c4adad
Radzhan <rbayhombaev@gmail.com>
2024-05-07 08:41:32 +0600

    resolve 1 task

--- task_1/index.tsx
+++ task_1/index.tsx
@@ -1,47 +1,66 @@
-import { Component } from 'react';
+import React from "react";
+import { Component, memo } from "react";
 
 type IUser = {
-    name: string
-    age: number
-}
+  name: string;
+  age: number;
+};
 
 type IProps = {
-    user: IUser
-}
+  user: IUser;
+};
 
 // functional component
-const FirstComponent = ({ name, age }: IUser) => (
-    <div>
-        my name is {name}, my age is {age}
-    </div>
-);
+const FirstComponent = memo(({ name, age }: IUser) => (
+  <div>
+    my name is {name}, my age is {age}
+  </div>
+));
 
 // functional component
 // Этот компонент является необязательным для выполнения задания, но продемонстрирует глубину знаний в React.
-const SecondComponent = ({ user: { name, age } }: IProps) => (
+
+export const SecondComponent = memo(({ user }: IProps) => {
+  return (
     <div>
-        my name is {name}, my age is {age}
+      my name is {user.name}, my age is {user.age}
     </div>
-);
+  );
+}, (prevProps, nextProps) => {
+  // Глубокое сравнение объекта user
+  return JSON.stringify(prevProps.user) === JSON.stringify(nextProps.user);
+});
 
 // class component
-class ThirdComponent extends Component<IUser> {
-    render() {
-        return (
-            <div>
-                my name is {this.props.name}, my age is {this.props.age}
-            </div>
-        )
-    }
+export class ThirdComponent extends Component<IUser> {
+  shouldComponentUpdate(nextProps: IUser) {
+    // Глубокое сравнение объекта user
+    return JSON.stringify(nextProps) !== JSON.stringify(this.props);
+  }
+
+  render() {
+    return (
+      <div>
+        my name is {this.props.name}, my age is {this.props.age}
+      </div>
+    );
+  }
 }
 
 // class component
-class FourthComponent extends Component<IProps> {
-    render() {
-        return (
-            <div>
-                my name is {this.props.user.name}, my age is {this.props.user.age}
-            </div>
-        )
-    }
+export class FourthComponent extends Component<IProps> {
+  shouldComponentUpdate(nextProps: IProps) {
+    // Глубокое сравнение объекта user
+    return JSON.stringify(nextProps.user) !== JSON.stringify(this.props.user);
+  }
+
+  render() {
+    console.log("FourthComponent has been updated");
+
+    return (
+      <div>
+        my name is {this.props.user.name}, my age is {this.props.user.age}
+      </div>
+    );
+  }
 }
